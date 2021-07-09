3e6bbabf47d16117b26f2d6f78309b521d7af68f
Syed Mazhar Ali Raza <mazharali.raza11@gmail.com>
2021-07-09 15:49:50 +0530

    added qualifications

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,3 +30,29 @@ skillHeader1.addEventListener('click', function () {
 skillHeader2.addEventListener('click', function () {
     skillContent2.classList.toggle('skills-open');
 });
+
+// Qualification
+let education = document.getElementById('education');
+let work = document.getElementById('work');
+let educationheader = document.getElementById('educationheader');
+let workheader = document.getElementById('workheader');
+educationheader.style.color = 'var(--first-colour)';
+
+educationheader.addEventListener('click', () => {
+    let condition1 = work.classList.contains('qualification-inactive');
+    if(!condition1){
+        education.classList.remove('qualification-inactive');
+        work.classList.add('qualification-inactive');
+        workheader.style.color = 'var(--text-colour)';
+        educationheader.style.color = 'var(--first-colour)';
+    }
+});
+workheader.addEventListener('click', () => {
+    let condition2 = education.classList.contains('qualification-inactive');
+    if(!condition2){
+        work.classList.remove('qualification-inactive');
+        education.classList.add('qualification-inactive');
+        educationheader.style.color = 'var(--text-colour)';
+        workheader.style.color = 'var(--first-colour)';
+    }
+});
